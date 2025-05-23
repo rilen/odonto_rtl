10d1f2fecb9635ed386f108357b0f7711a8add80
Rilen T.L. <rilen.lima@gmail.com>
2025-05-23 19:00:40 -0300

    Create webpack.config.js

---
+++ frontend/webpack.config.js
@@ -0,0 +1,33 @@
+const path = require("path");
+
+module.exports = {
+  mode: "development",
+  entry: "./src/index.js",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"],
+            plugins: [
+              "@babel/plugin-proposal-class-properties",
+              "@babel/plugin-proposal-optional-chaining"
+            ]
+          }
+        }
+      }
+    ]
+  },
+  devServer: {
+    static: path.join(__dirname, "public"),
+    compress: true,
+    port: 3000
+  }
+};
