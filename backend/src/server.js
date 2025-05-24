fa690f4a41b53d76ebf67aaa47c5c5536f5e9f5a
Rilen T.L. <rilen.lima@gmail.com>
2025-05-24 14:05:14 -0300

    Update server.js

--- backend/src/server.js
+++ backend/src/server.js
@@ -5,14 +5,13 @@ const authMiddleware = require('./middleware/auth');
 
 const app = express();
 
-app.use(cors());
+app.use(cors({ origin: 'http://localhost:3001' })); // Allow frontend origin
 app.use(express.json());
 
 app.get('/', (req, res) => {
   res.send('API Odonto RTL funcionando!');
 });
 
-// Rotas
 app.use('/api/auth', require('./routes/auth'));
 app.use('/api/users', require('./routes/users'));
 app.use('/api/dashboard', authMiddleware, require('./routes/dashboard'));
